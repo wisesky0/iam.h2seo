463faa67e349e283b20888310498abf4b25260fb
wisesky0 <wisesky0@gmail.com>
2022-11-28 01:29:57 +0900

    First

---
+++ utils/dependency-copy/src/main/java/iam/h2s/utilities/dependencycopy/DependencyCopyApplication.java
@@ -0,0 +1,13 @@
+package iam.h2s.utilities.dependencycopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyCopyApplication.class, args);
+	}
+
+}
---
+++ utils/dependency-copy/src/test/java/iam/h2s/utilities/dependencycopy/DependencyCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.h2s.utilities.dependencycopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
